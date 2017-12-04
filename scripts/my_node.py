8d967236f4bb1a4f7c0fa8a5fcfdeed4d0567c3c
Laval Martinez <maud.laval.9@hotmail.fr>
2017-12-04 18:24:14 +0100

    Martinez Laval TP

---
+++ scripts/my_node.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Float64
+
+rospy.init_node('my_node')
+pub = rospy.Publisher('laser_velocity_controller/command', Float64, queue_size=10)
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+	msg = Float64()
+	msg.data = 5
+	pub.publish(msg)
+	rate.sleep()
